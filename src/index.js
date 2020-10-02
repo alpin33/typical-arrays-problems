de3c629300adef096fa864aee5e2e7ed6ca40a9c
alpin33 <70812467+alpin33@users.noreply.github.com>
2020-10-02 12:22:55 +0100

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length === 0) {
+      return 0;
+  } else {
+      return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+      return 0;
+  } else {
+      return Math.max(...array);
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length === 0) {
+      return 0;
+  } else {
+      const avrgNum = array.reduce((a, b) => a + b) / array.length;
+      return avrgNum;
+  }
 }
